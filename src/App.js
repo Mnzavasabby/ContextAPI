9cd455d58c6dc370e8b66814055c28bc07196ab8
Mnzavasabby <rashidsabitina@gmail.com>
2024-09-13 14:57:45 +0300

    added contexts component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import BookLists from "./Components/BookLists";
 import Navbar from "./Components/Navbar";
+import ThemeContextProvider from "./Contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
+      <ThemeContextProvider>
         <Navbar />
         <BookLists />
+        </ThemeContextProvider>
     </div>
   );
 }
