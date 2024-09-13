d0f087f29b1852f0c91d66efec4dee80965bb1bc
Mnzavasabby <rashidsabitina@gmail.com>
2024-09-13 14:19:28 +0300

    added navbar and booklist components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import BookLists from "./Components/BookLists";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <BookLists />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BookLists.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+class BookLists extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>Hawa the bus driver</li>
+                    <li>Passed like a shadow</li>
+                    <li>Three Suits one Husband</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default BookLists;
